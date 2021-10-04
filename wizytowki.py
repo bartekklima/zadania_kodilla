199114d6be751dbeee772578e45f5d74af6e14a9
bartek <bartek.klima@onet.pl>
2021-10-04 19:57:46 +0200

    wizytówka dziedziczenie

--- wizytowki.py
+++ wizytowki.py
@@ -1,10 +1,22 @@
-class namecard:
-    def __init__(self,name,surname,email,firm,position):
+class BaseContact:
+    def __init__(self,name,surname,email,number):
         self.name = name
         self.surname = surname
         self.email = email
-        self.firm = firm
+        self.number = number
+    
+    @property
+    def contact(self):
+        print(f"Wybieram numer {self.number} i dzwonię do {self.name} {self.surname}")
+    @property
+    def label_length(self):
+        print("Ilość liter imienia = %d i nazwiska = %d" % (len(self.name),len(self.surname)))
+
+class BusinessContact(BaseContact):
+    def __init__(self, position, firm, *arg, **kwarg):
+        super().__init__(*arg, **kwarg)
         self.position = position
+        self.firm = firm
 
 S_Czarnecki = namecard("Szymon","Czarnecki","SzymonCzarnecki@dayrep.com","Standard Food","Fire fighter")
 K_Wieczorek = namecard("Kasper","Wieczorek","KasperWieczorek@jourrapide.com","Peter Reeves","Architect")
@@ -16,3 +28,6 @@ namecard_pack = [M_Borkowski, S_Czarnecki, L_Dudek, J_Nowak, K_Wieczorek]
 
 for i in namecard_pack:
     print("{:10}{:12}{}".format(i.name, i.surname, i.email))
+
+S_Czarnecki.contact
+S_Czarnecki.label_length   
