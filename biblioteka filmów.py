f14ef58e8415756d115e0b10aaba85193ca65bf3
bartek <bartek.klima@onet.pl>
2021-10-09 00:01:48 +0200

    biblioteka filmów - funkcja szukanie

--- biblioteka filmów.py
+++ biblioteka filmów.py
@@ -12,6 +12,10 @@ class Film:                                                                 # Tw
         self.number_of_plays += 1
         return "{} ({})".format(self.title, self.publication_date)
 
+    def __repr__(self):
+        return '%s' %self.title
+
+
 class Serial(Film):
     def __init__(self, episode_number, season_number, *arg, **kwarg):
         super().__init__(*arg, **kwarg)
@@ -24,59 +28,94 @@ class Serial(Film):
 
 
 
-def list_maker(lista):                                           # Przypisywaanie danych do film i serial
+
+def list_maker(a):                                           # Przypisywaanie danych do film i serial
                                                                  # oraz umieszcznie ich w jednej liscie
-    kind=input('enter kind: ')                                       
-    Title = input('Title: ')
-    Publication_date = int(input('Publication_date: '))
-    Type = input('Type: ')
-    Number_of_plays = int(input('Number_of_plays: '))
+    def body_list_maker(lista):
+        kind=input('enter kind: ')                                       
+        Title = input('Title: ')
+        Publication_date = int(input('Publication_date: '))
+        Type = input('Type: ')
+        Number_of_plays = int(input('Number_of_plays: '))
+        
+        while kind != 'film' and kind != 'serial':
+            kind=input ('wrong kind. Choose between "film" or "serial": ')
+
+        if kind == 'film':
+            film = Film(                                    
+                title=Title, 
+                publication_date=Publication_date, 
+                type=Type,
+                number_of_plays=Number_of_plays)
+
+            lista.append(film)
+
+        elif kind == 'serial':
+            Episode_number=int(input('episode_number: '))
+            Season_number=int(input("season_number: "))
+            
+            serial = Serial(
+                title=Title, 
+                publication_date=Publication_date, 
+                type=Type,
+                number_of_plays=Number_of_plays,
+                episode_number=Episode_number, 
+                season_number=Season_number)
+            
+            lista.append(serial)
     
-    while kind != 'film' and kind != 'serial':
-        kind=input ('wrong kind. Choose between "film" or "serial": ')
+    body_list_maker(a)             
+    while ""==input('Would you like add one more film - PRESS ENTER (enter something and press enter to skip)'):  # Powtórzenie funkji
+        body_list_maker(a)
+ 
 
-    if kind == 'film':
-        Title = Film(                                    
-            title=Title, 
-            publication_date=Publication_date, 
-            type=Type,
-            number_of_plays=Number_of_plays)
 
-        lista.append(Title)
 
-    elif kind == 'serial':
-        Episode_number=int(input('episode_number: '))
-        Season_number=int(input("season_number: "))
-        
-        Title = Serial(
-            title=Title, 
-            publication_date=Publication_date, 
-            type=Type,
-            number_of_plays=Number_of_plays,
-            episode_number=Episode_number, 
-            season_number=Season_number)
-        
-        lista.append(Title)
+def list_filter(lista):             # Funkcja rozdzielająca bibliotekę filmów i seriali
+    films = []
+    serials = []
+    for i in lista:
+        if type(i)==Film:
+            films.append(i)
+        else:
+            serials.append(i)
+    
+    # try:
+    #     for i in films:
+    #         print('Films',i)
+    # except:
+    #     pass
+    # try:
+    #     for i in serials:
+    #         print('Serials',i)
+    # except:
+    #     pass
 
-    return lista
+    return films, serials
+    
 
-# film = Film(                                   
-#     title="The Simpsons", 
-#     publication_date=2013, 
-#     type="comedy",
-#     number_of_plays= 2) 
+def search(lista):                # Szukanie video po nazwie w bibliotece
+    name = input("Enter name of looking for video: ")
+    for i in lista:
+        if i.title == name:
+            index = lista.index(i)
+            return lista[index]
 
-# serial = Serial(
-#     title ="The Simpsons", 
-#     episode_number=5, 
-#     season_number=1,
-#     publication_date=1903, 
-#     type="comedy",
-#     number_of_plays= 2)
 
-a=[]
-list_maker(a)
 
-for i in a:
-    i.play()
-    print(i, '\n', i.number_of_plays)
+
+library=[]                                # Tworzę bibliotekę filmów i seriali
+list_maker(library)
+print(library)
+
+# for i in a:                           # Odtwarzam wszystkie filmy i serial z biblioteki
+#     i.play()
+#     print(i, '\n', i.number_of_plays, '\n')
+
+
+sorted_library = list_filter(library)       # Rozdzielam bibliotekę filmów i seriali
+films, serials = sorted_library
+print('\n',films, serials)
+
+looking_video = search(library)             # Szukanie video po nazwie w bibliotece
+print(looking_video)
