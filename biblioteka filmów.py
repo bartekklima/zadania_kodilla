5fbccef8d7b1e085c864cea7b7facbc1cb133dec
bartek <bartek.klima@onet.pl>
2021-10-08 11:19:13 +0200

    biblioteka filmów - podstawa

--- biblioteka filmów.py
+++ biblioteka filmów.py
@@ -10,7 +10,7 @@ class Film:                                                                 # Tw
     
     def __str__(self):
         self.number_of_plays += 1
-        return "{} ({})".format(serial.title, serial.publication_date)
+        return "{} ({})".format(self.title, self.publication_date)
 
 class Serial(Film):
     def __init__(self, episode_number, season_number, *arg, **kwarg):
@@ -20,35 +20,47 @@ class Serial(Film):
 
     def __str__(self):
         self.number_of_plays += 1
-        return "{} S{:02}E{:02}".format(serial.title, serial.season_number, serial.episode_number)
+        return "{} S{:02}E{:02}".format(self.title, self.season_number, self.episode_number)
 
-def lista(kind):
-    Title = input('Title: '), 
-    Publication_date = input('Publication_date: '), 
-    Type = input('Type: '), 
-    Number_of_plays = input('Number_of_plays: ')
+
+
+def list_maker(lista):                                           # Przypisywaanie danych do film i serial
+                                                                 # oraz umieszcznie ich w jednej liscie
+    kind=input('enter kind: ')                                       
+    Title = input('Title: ')
+    Publication_date = int(input('Publication_date: '))
+    Type = input('Type: ')
+    Number_of_plays = int(input('Number_of_plays: '))
     
+    while kind != 'film' and kind != 'serial':
+        kind=input ('wrong kind. Choose between "film" or "serial": ')
+
     if kind == 'film':
-        film = Film(                                    # Przypisywanie klas do zmiennych
+        Title = Film(                                    
             title=Title, 
             publication_date=Publication_date, 
             type=Type,
             number_of_plays=Number_of_plays)
 
+        lista.append(Title)
+
     elif kind == 'serial':
-        # serial = Serial(
-        #     title=input('Title: '), 
-        #     publication_date=input('Publication_date: '), 
-        #     type=input('Type: '),
-        #     number_of_plays=input('Number_of_plays: '), 
-        #     episode_number=input('episode_number: '), 
-        #     season_number=input("season_number: "))
-        pass
-    else:
-        raise NameError ('wroeng kind. Choose between "film" or "serial"')
+        Episode_number=int(input('episode_number: '))
+        Season_number=int(input("season_number: "))
+        
+        Title = Serial(
+            title=Title, 
+            publication_date=Publication_date, 
+            type=Type,
+            number_of_plays=Number_of_plays,
+            episode_number=Episode_number, 
+            season_number=Season_number)
+        
+        lista.append(Title)
 
+    return lista
 
-# film = Film(                                    # Przypisywanie klas do zmiennych
+# film = Film(                                   
 #     title="The Simpsons", 
 #     publication_date=2013, 
 #     type="comedy",
@@ -62,4 +74,9 @@ def lista(kind):
 #     type="comedy",
 #     number_of_plays= 2)
 
-lista(input('enter kind: '))
+a=[]
+list_maker(a)
+
+for i in a:
+    i.play()
+    print(i, '\n', i.number_of_plays)
